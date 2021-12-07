92fc6500e8812ac89e117eaaf570c92731d4597d
Mouleeswaran12 <95650245+Mouleeswaran12@users.noreply.github.com>
2021-12-07 12:39:37 +0530

    1-page1

--- app/src/main/java/com/example/pddetect/MainActivity.java
+++ app/src/main/java/com/example/pddetect/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class MainActivity extends AppCompatActivity {
+    LottieAnimationView doc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        doc=findViewById(R.id.doctorlotties);
+        doc.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+
     }
 }
