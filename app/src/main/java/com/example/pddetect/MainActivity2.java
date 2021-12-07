0cd4a73e242f4717507a7e67f19fbc523676ebde
Mouleeswaran12 <95650245+Mouleeswaran12@users.noreply.github.com>
2021-12-07 21:10:59 +0530

    page2-1

--- app/src/main/java/com/example/pddetect/MainActivity2.java
+++ app/src/main/java/com/example/pddetect/MainActivity2.java
@@ -2,19 +2,24 @@ package com.example.pddetect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
    // TextView pddetect;
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        b=findViewById(R.id.submit);
        // pddetect=findViewById(R.id.text);
         //pddetect.animate().translationY(-1000).setDuration(1000).setStartDelay(4000);
         final AutoCompleteTextView customerAutoTV = findViewById(R.id.customerTextView);
@@ -27,6 +32,13 @@ public class MainActivity2 extends AppCompatActivity {
 
         //Set adapter
         customerAutoTV.setAdapter(adapter);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2=new Intent(MainActivity2.this,MainActivity3.class);
+                startActivity(intent2);
+            }
+        });
     }
     private ArrayList<String> getCustomerList()
     {
