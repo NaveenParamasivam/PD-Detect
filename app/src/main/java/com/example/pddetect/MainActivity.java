67600540b31e5123d279fd8e02baf140c5a6f5e4
naveen <naveenneevan73@gmail.com>
2021-12-07 16:04:11 +0530

    splash screen

--- app/src/main/java/com/example/pddetect/MainActivity.java
+++ app/src/main/java/com/example/pddetect/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.pddetect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView bg;
     LottieAnimationView doc;
 
     @Override
@@ -14,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         doc=findViewById(R.id.doctorlotties);
-        doc.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+        bg=findViewById(R.id.bg);
+        bg.animate().translationY(-3000).setDuration(1000).setStartDelay(4000);
+        doc.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
 
     }
 }
