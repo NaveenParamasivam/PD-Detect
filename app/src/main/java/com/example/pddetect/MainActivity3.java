048aefab0c366506b7a1d86f1543bf903202e76d
Mouleeswaran12 <95650245+Mouleeswaran12@users.noreply.github.com>
2021-12-07 22:04:21 +0530

    page2-1

--- app/src/main/java/com/example/pddetect/MainActivity3.java
+++ app/src/main/java/com/example/pddetect/MainActivity3.java
@@ -2,13 +2,34 @@ package com.example.pddetect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity3 extends AppCompatActivity {
+    Button bpd,bse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        bpd=findViewById(R.id.button);
+        bse=findViewById(R.id.button1);
+        bpd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3=new Intent(MainActivity3.this,MainActivity6.class);
+                startActivity(intent3);
+            }
+        });
+        bse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent4=new Intent(MainActivity3.this,MainActivity9.class);
+                startActivity(intent4);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pddetect/MainActivity6.java
+++ app/src/main/java/com/example/pddetect/MainActivity6.java
@@ -2,7 +2,9 @@ package com.example.pddetect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity6 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent1 =new Intent(MainActivity6.this,MainActivity7.class);
+                startActivity(intent1);
+                finish();
+            }
+        },5000);
     }
 }
