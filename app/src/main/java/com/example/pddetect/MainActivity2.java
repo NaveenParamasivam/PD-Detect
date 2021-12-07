b69b59ba269ac48dd7ffa23a401dcc8576217679
Mouleeswaran12 <95650245+Mouleeswaran12@users.noreply.github.com>
2021-12-07 21:03:16 +0530

    page2-1

--- app/src/main/java/com/example/pddetect/MainActivity2.java
+++ app/src/main/java/com/example/pddetect/MainActivity2.java
@@ -3,7 +3,11 @@ package com.example.pddetect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
    // TextView pddetect;
@@ -13,5 +17,22 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
        // pddetect=findViewById(R.id.text);
         //pddetect.animate().translationY(-1000).setDuration(1000).setStartDelay(4000);
+        final AutoCompleteTextView customerAutoTV = findViewById(R.id.customerTextView);
+
+        // create list of customer
+        ArrayList<String> customerList = getCustomerList();
+
+        //Create adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity2.this, android.R.layout.simple_spinner_item, customerList);
+
+        //Set adapter
+        customerAutoTV.setAdapter(adapter);
+    }
+    private ArrayList<String> getCustomerList()
+    {
+        ArrayList<String> customers = new ArrayList<>();
+        customers.add("Male");
+        customers.add("Female");
+        return customers;
     }
 }
