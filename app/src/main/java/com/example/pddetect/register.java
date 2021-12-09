9e4353ec8148602f074f220e39736c2312a294d9
Mouleeswaran12 <95650245+Mouleeswaran12@users.noreply.github.com>
2021-12-09 23:37:59 +0530

    shared preferences

--- app/src/main/java/com/example/pddetect/register.java
+++ app/src/main/java/com/example/pddetect/register.java
@@ -3,23 +3,33 @@ package com.example.pddetect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class register extends AppCompatActivity {
     Button submit;
+    TextView Name,Age,Height,Weight;
+    TextView Gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
         submit=findViewById(R.id.submit);
-
-        final AutoCompleteTextView genderauto = findViewById(R.id.customerTextView);
+        Name=findViewById(R.id.name);
+        Age=findViewById(R.id.age);
+        Gender=findViewById(R.id.spinnertext);
+        Height=findViewById(R.id.height);
+        Weight=findViewById(R.id.weight);
+        final AutoCompleteTextView genderauto = findViewById(R.id.spinnertext);
 
         ArrayList<String> genderList = getCustomerList();
 
@@ -29,6 +39,16 @@ public class register extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                SharedPreferences profiledata=getSharedPreferences("profile",MODE_PRIVATE);
+                SharedPreferences.Editor profileedit=profiledata.edit();
+                profileedit.putString("name",Name.getText().toString());
+                profileedit.putInt("age",Integer.parseInt(Age.getText().toString()));
+                profileedit.putString("gender",Gender.getText().toString());
+                profileedit.putFloat("height",Float.parseFloat(Height.getText().toString()));
+                profileedit.putFloat("weight",Float.parseFloat(Weight.getText().toString()));
+                profileedit.commit();
+                //Toast.makeText(register.this, Name.getText().toString()+" "+Age.getText().toString()+" "+Gender.getText().toString()+" "+Height.getText().toString()+" "+Weight.getText().toString(), Toast.LENGTH_SHORT).show();
                 Intent homeintent0=new Intent(register.this, homepage.class);
                 startActivity(homeintent0);
             }
