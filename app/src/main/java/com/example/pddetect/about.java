57f6f75cf438096ac7148e22233502a5d4a7bb41
Mouleeswaran12 <95650245+Mouleeswaran12@users.noreply.github.com>
2021-12-10 16:31:10 +0530

    Severity result page

--- app/src/main/java/com/example/pddetect/about.java
+++ app/src/main/java/com/example/pddetect/about.java
@@ -5,17 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class about extends AppCompatActivity {
     LottieAnimationView homeicon1;
-
+    TextView aboutapp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
         homeicon1=findViewById(R.id.home1);
+        aboutapp=findViewById(R.id.aboutdescription);
+        aboutapp.setText("Parkinson’s Disease (PD) is a disorder of the central nervous system that affects movement, often including tremors. We have designed a system that uses deep learning to detect Parkinson’s Disease at an earlier stage.");
         homeicon1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/pddetect/severityresult.java
+++ app/src/main/java/com/example/pddetect/severityresult.java
@@ -2,10 +2,16 @@ package com.example.pddetect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class severityresult extends AppCompatActivity {
+    TextView resultscore;
+    Button homenavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,15 @@ public class severityresult extends AppCompatActivity {
         setContentView(R.layout.severityresult);
         String data = getIntent().getStringExtra("result");
         Toast.makeText(getApplicationContext(), data, Toast.LENGTH_LONG).show();
+        resultscore=findViewById(R.id.textView3);
+        homenavigation=findViewById(R.id.button2);
+        resultscore.setText(data);
+        homenavigation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homenavigationintent=new Intent(severityresult.this,homepage.class);
+                startActivity(homenavigationintent);
+            }
+        });
     }
 }
